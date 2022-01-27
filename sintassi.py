d75fc95a4996b2dc3c750240f57639ad8a5d03b4
Daniele Porcari <64744714+danielePy@users.noreply.github.com>
2022-01-27 23:04:38 +0100

    Add files via upload

---
+++ sintassi.py
@@ -0,0 +1,9 @@
+path="C:\\Users\\es-523-8871\\costruttore\\ui"
+mainwin="mainwindow.py"
+
+A="#-*-coding: utf-8 -*-\n#base scripts generated with COSTRUTTORE\n#created by Porcari Daniele\n"
+A=A+"import sys\nfrom PyQt5.QtWidgets import (QApplication, QDialog, QMainWindow, QMessageBox)\n"
+A=A+"from PyQt5.uic import loadUi\nfrom PyQt5 import QtCore, QtGui\n"
+INIT="\tdef __init__(self,parent=None):\n\t\tsuper().__init__(parent)\n\t\tself.setupUi(self)\n\t\tself.connectSignalSlots()\n"
+INIT=INIT+"\n\tdef connectSignalSlots(self):\n"
+START="if __name__ == \"__main__\":\n\tapp = QApplication(sys.argv)\n\twin = Window()\n\twin.show()\n\tsys.exit(app.exec())"
