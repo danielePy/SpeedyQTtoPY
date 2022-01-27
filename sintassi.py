5d3d7a43888f870e249d70dba790d7ae0cc705fd
Daniele Porcari <64744714+danielePy@users.noreply.github.com>
2022-01-27 23:06:17 +0100

    Update sintassi.py

--- sintassi.py
+++ sintassi.py
@@ -1,7 +1,7 @@
 path="C:\\Users\\es-523-8871\\costruttore\\ui"
 mainwin="mainwindow.py"
 
-A="#-*-coding: utf-8 -*-\n#base scripts generated with COSTRUTTORE\n#created by Porcari Daniele\n"
+A="#-*-coding: utf-8 -*-\n#base scripts generated with speedyQTtoPY\n#created by Porcari Daniele\n"
 A=A+"import sys\nfrom PyQt5.QtWidgets import (QApplication, QDialog, QMainWindow, QMessageBox)\n"
 A=A+"from PyQt5.uic import loadUi\nfrom PyQt5 import QtCore, QtGui\n"
 INIT="\tdef __init__(self,parent=None):\n\t\tsuper().__init__(parent)\n\t\tself.setupUi(self)\n\t\tself.connectSignalSlots()\n"
